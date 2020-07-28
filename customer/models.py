ddd580663c8b4bd1168bb0a22d4920ce5c430734
4lexRossi <devalexrossi@gmail.com>
2020-07-28 20:53:28 -0300

    update method add

--- customer/models.py
+++ customer/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -30,6 +31,8 @@ class Customer(models.Model):
   def get_full_addres(self):
     return f"{self.city} / {self.state}"
 
+  def get_absolute_url(self):
+    return reverse("customer:customer-update", kwargs={"id": self.id})
   
   class Meta:
     db_table = "customer"
--- customer/views.py
+++ customer/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse
+from django.shortcuts import get_object_or_404
+
 from .models import Customer
 from .forms import CustomerForm
 
@@ -12,6 +14,20 @@ class CustomerListView(ListView):
 class CustomerCreateView(CreateView):
   template_name = "customer/customer.html"
   form_class = CustomerForm
+
+  def form_valid(self, form):
+    return super().form_valid(form)
+
+  def get_success_url(self):
+    return reverse("customer:customer-list")
+
+class CustomerUpdateView(UpdateView):
+  template_name = "customer/customer.html"
+  form_class = CustomerForm
+
+  def get_object(self):
+    id = self.kwargs.get("id")
+    return get_object_or_404(Customer, id=id)
 
   def form_valid(self, form):
     return super().form_valid(form)
