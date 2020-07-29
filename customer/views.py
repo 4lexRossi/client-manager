0c359fd0ee47e999544b67efa5d4b2973277e5cb
4lexRossi <devalexrossi@gmail.com>
2020-07-28 21:07:20 -0300

    add delete method

--- customer/views.py
+++ customer/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
 
@@ -31,6 +31,14 @@ class CustomerUpdateView(UpdateView):
 
   def form_valid(self, form):
     return super().form_valid(form)
+
+  def get_success_url(self):
+    return reverse("customer:customer-list")
+
+class CustomerDeleteView(DeleteView):
+  def get_object(self):
+    id = self.kwargs.get("id")
+    return get_object_or_404(Customer, id=id)  
 
   def get_success_url(self):
     return reverse("customer:customer-list")
