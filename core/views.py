17760aeab488f08e900cb1e191133d9c6ec0f162
4lexRossi <devalexrossi@gmail.com>
2020-07-28 22:35:06 -0300

    add login page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
+def login(request):
+  return render(request, "login.html")
+
+def submit_login(request):
+  if request.POST:
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(username=username, password=password)
+    if user:
+      login(request, user)
+      return redirect("index")
+    else:
+      messages.error(request, "Usuário/Senha inválidos. Tente novamente")
+  return redirect("login")
